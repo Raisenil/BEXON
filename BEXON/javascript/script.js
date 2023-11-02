b1d1283602ccb4d23691f0715d725ac90ae2224f
Raisul Islam Niloy <raisulislamniloy47@gmail.com>
2023-11-02 21:55:33 +0600

    review page design done

--- BEXON/javascript/script.js
+++ BEXON/javascript/script.js
@@ -43,3 +43,22 @@ $(document).ready(function() {
         }
     });
 });
+
+
+// Navbar active
+
+document.addEventListener('scroll', function () {
+    const sections = document.querySelectorAll('section');
+    const navbarLinks = document.querySelectorAll('.navbar a');
+
+    sections.forEach(function (section, index) {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
+            navbarLinks[index].classList.add('active');
+        } else {
+            navbarLinks[index].classList.remove('active');
+        }
+    });
+});
