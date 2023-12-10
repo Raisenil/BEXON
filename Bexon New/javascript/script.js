3e1aa3506fbb9e7df19fe1826d0fe6e1397857f6
Raisul Islam Niloy <raisulislamniloy47@gmail.com>
2023-12-11 00:22:46 +0600

    Process section start

--- Bexon New/javascript/script.js
+++ Bexon New/javascript/script.js
@@ -15,3 +15,23 @@ document.addEventListener('scroll', function () {
     });
 });
 
+// toggle icon
+$(document).ready(function() {
+    $(".toggle").click(function(e) {
+        e.preventDefault();
+        var text = $(this).siblings(".text");
+        var icon = $(this).find("img");
+        var content = $(this).closest(".content");
+
+        if (text.is(":hidden")) {
+            text.slideDown();
+            icon.attr("src", "Assets/icons/reviews-Icon.svg");
+            content.css("background-color", "#2F2927");
+        } else {
+            text.slideUp();
+            icon.attr("src", "Assets/icons/reviews-Info.svg");
+            content.css("background-color", "transparent");
+        }
+    });
+});
+
